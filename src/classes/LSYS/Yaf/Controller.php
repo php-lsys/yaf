b795f263eaf883a0305547a031341db3c2711a51
shanliu <shanliu@msn.com>
2020-01-22 10:07:53 +0800

    ..

--- src/classes/LSYS/Yaf/Controller.php
+++ src/classes/LSYS/Yaf/Controller.php
@@ -3,12 +3,19 @@ namespace LSYS\Yaf;
 use LSYS\Yaf\ObjectRender\DataResult;
 use LSYS\Yaf\ObjectRender\Output;
 abstract class Controller extends \Yaf\Controller_Abstract{
+    /**
+     * 禁用默认渲染视图
+     * {@inheritDoc}
+     * @see \Yaf\Controller_Abstract::init()
+     */
+    public function init(){
+        \Yaf\Application::app()->getDispatcher()->disableView();
+    }
 	/**
 	 * 自适应输出数据
 	 * @param mixed $data
 	 */
 	protected function displayData($data=null,$format=null){
-		\Yaf\Application::app()->getDispatcher()->disableView();
 		if (!$data instanceof DataResult) $data=DataResult::factory()->data($data);
 		(new Output(\LSYS\ObjectRender\DI::get()->objectRender()->setObject($data), $this->_response))->render($format);
 		return false;
